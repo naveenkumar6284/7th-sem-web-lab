b9fc3906fc3d64de2cd42a88f0306d96bab6472b
Lucifer628 <73075519+Lucifer628@users.noreply.github.com>
2021-01-16 13:03:20 +0530

    Add files via upload

---
+++ program10/p10.php
@@ -0,0 +1,37 @@
+<?php
+	$connect = mysqli_connect("localhost","root","","p10");
+
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL:-". mysqli_connect_error();
+}
+
+if(isset($_POST['submit']))
+{
+$usn=$_POST['usn'];
+$name=$_POST['name'];
+$email=$_POST['email'];
+$mob=$_POST['mob'];
+$s="insert into stud values('$usn','$name','$email',$mob)";
+$ins=mysqli_query($connect,$s);
+
+if($ins)
+{
+echo "
+<script>
+alert('Inserted Successfully');
+window.location.href='Program10(sorting).php';
+</script>
+";
+}
+else
+{
+echo mysqli_error($connect);
+echo "
+<script>
+alert('Error in insertion');
+</script>
+";
+}
+}
+?>
